03f010eefb4ba1b44fb330c6a703544dcff1189b
Pinguladora <mec@gmail.com>
2026-03-15 00:05:43 +0100

    feat(tofu): update GitHub repository module
    add dynamic blocks to account for edge cases (e.g. PRO requirement,
    Pages without content)
    fix license template use case
    add template repo use case

--- modules/github/repository/main.tf
+++ modules/github/repository/main.tf
@@ -26,50 +26,68 @@ resource "github_repository" "this" {
   squash_merge_commit_message = "COMMIT_MESSAGES"
 
   # Archive
-  archive_on_destroy = true
+  archive_on_destroy = false
+
+  # Template
+  dynamic "template" {
+    for_each = var.is_template ? [1] : []
+    content {
+      owner                = var.template_owner
+      repository           = "${var.name}-template"
+      include_all_branches = false
+    }
+  }
 
   # GitHub Pages
-  pages {
-    build_type = "workflow"
-    source {
-      branch = "main"
-      path   = "/docs/website"
+  dynamic "pages" {
+    for_each = var.enable_pages ? [1] : []
+    content {
+      build_type = "workflow"
+      source {
+        branch = "main"
+        path   = "/docs"
+      }
     }
   }
 
   # Security
   vulnerability_alerts = true
   ## Advanced Security only for public repos
-  security_and_analysis {
-    advanced_security {
-      status = var.visibility == "public" ? "enabled" : "disabled"
-    }
-    code_security {
-      status = var.visibility == "public" ? "enabled" : "disabled"
-    }
-    secret_scanning {
-      status = var.visibility == "public" ? "enabled" : "disabled"
-    }
-    secret_scanning_push_protection {
-      status = var.visibility == "public" ? "enabled" : "disabled"
-    }
-    secret_scanning_ai_detection {
-      status = var.visibility == "public" ? "enabled" : "disabled"
-    }
-    secret_scanning_non_provider_patterns {
-      status = var.visibility == "public" ? "enabled" : "disabled"
+  dynamic "security_and_analysis" {
+    for_each = (var.visibility != "public" && var.has_advanced_security) ? [1] : []
+    content {
+      advanced_security {
+        status = "enabled"
+      }
+      code_security {
+        status = "enabled"
+      }
+      secret_scanning {
+        status = "enabled"
+      }
+      secret_scanning_push_protection {
+        status = "enabled"
+      }
+      secret_scanning_ai_detection {
+        status = "enabled"
+      }
+      secret_scanning_non_provider_patterns {
+        status = "enabled"
+      }
     }
   }
 }
 
 # Trunk Based Development (TBD)
 resource "github_branch_default" "default" {
-  repository = github_repository.this.node_id
+  repository = github_repository.this.name
   branch     = "main"
 }
 
 # CIS Requirement, enforce Branch Protection on default branch
+# Private: only available on PRO, Team and Enterprise plans
 resource "github_branch_protection" "default" {
+  count               = (var.visibility == "public" || var.owner_has_pro) ? 1 : 0
   repository_id       = github_repository.this.node_id
   pattern             = "main"
   enforce_admins      = true
--- modules/github/repository/variables.tf
+++ modules/github/repository/variables.tf
@@ -1,3 +1,9 @@
+variable "owner_has_pro" {
+  description = "Set true if the repo owner has GitHub Pro, Teams or Enterprise plan allowing private branch protection."
+  type        = bool
+  default     = true
+}
+
 variable "name" {
   description = "The name of the repository"
   type        = string
@@ -15,14 +21,30 @@ variable "visibility" {
   default     = "private"
   validation {
     condition     = contains(["public", "private", "internal"], var.visibility)
-    error_message = "Visibility must be either 'public', 'private', or 'internal'."
+    error_message = "Visibility must be either 'public', 'private', or 'internal' if it is within an Enterprise organization."
   }
+}
+
+variable "enable_pages" {
+  description = "Whether to activate GitHub Pages"
+  type        = bool
+  default     = false
 }
 
 variable "is_template" {
   description = "Whether this repository is a template repository"
   type        = bool
   default     = false
+}
+
+variable "template_owner" {
+  description = "Name of owner (user or organization) for the repository"
+  type        = string
+  default     = null
+  validation {
+    condition     = !var.is_template || trimspace(var.template_owner) == "" || trimspace(var.template_owner) == null
+    error_message = "Owner must be specified when creating a template repo"
+  }
 }
 
 variable "topics" {
@@ -46,15 +68,21 @@ variable "gitignore_template" {
 variable "license_template" {
   description = "The name of the license template to apply when creating the repository. See https://github.com/github/choosealicense.com/tree/gh-pages/_licenses for available templates."
   type        = string
-  default     = "apache-2.0"
+  default     = ""
   validation {
-    condition     = var.visibility == "public" || trimspace(var.license_template) == "" || trimspace(var.license_template) == null
+    condition     = var.visibility != "public" || trimspace(var.license_template) == "" || trimspace(var.license_template) == null
     error_message = "Visibility must be 'public' in order to apply a license template."
   }
 }
 
 variable "allow_merge_commit" {
   description = "Whether to allow merge commits when merging pull requests"
+  type        = bool
+  default     = false
+}
+
+variable "has_advanced_security" {
+  description = "Whether there is a GitHub Advance Security license"
   type        = bool
   default     = false
 }
